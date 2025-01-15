d865ed8abafc9a1033d57b0de5ff430da9493645
TAngell11 <ryanjue11@gmail.com>
2025-01-15 16:44:22 +0100

    Create games.py

---
+++ bots/games.py
@@ -0,0 +1,27 @@
+import random
+import discord
+from discord.ext import commands
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+token = os.getenv('DISCORD_TOKEN')
+
+intents = discord.Intents.all()
+bot = commands.Bot(command_prefix='!', intents=intents)
+
+@commands.command()
+async def roll(ctx):
+  result = random.randint(1, 6)
+  await ctx.send(f'{ctx.author.name} lance un dé et obtient : {result}')
+
+@bot.hybrid_command()
+async def pileouface(ctx):
+  result = random.choice(['pile', 'face'])
+  await ctx.send(f'Résulat obtenu : {result}')
+
+@bot.event
+async def on_ready():
+  await bot.tree.sync()
+
+bot.run(token=token) 
